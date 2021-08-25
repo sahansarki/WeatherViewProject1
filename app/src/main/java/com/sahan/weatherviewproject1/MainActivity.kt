28d53b5b447a061f9769d2eb3a4a76cc80db8c3b
sahan <sahancansarki@gmail.com>
2021-08-26 00:16:58 +0300

    Second Comment

--- app/src/main/java/com/sahan/weatherviewproject1/MainActivity.kt
+++ app/src/main/java/com/sahan/weatherviewproject1/MainActivity.kt
@@ -2,10 +2,15 @@ package com.sahan.weatherviewproject1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
